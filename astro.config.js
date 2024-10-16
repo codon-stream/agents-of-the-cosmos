46a6a7f4444c8b89e764d8e2df5f981855a7ff7e
Ivan Dwane <dwane.ivan@gmail.com>
2024-10-17 02:17:37 +0800

    build: add react

--- astro.config.js
+++ astro.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -9,5 +10,5 @@ export default defineConfig({
   security: {
     checkOrigin: true,
   },
-  integrations: [tailwind()],
+  integrations: [react(), tailwind()],
 });
