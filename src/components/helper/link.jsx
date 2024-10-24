4b31c9a2537a43a5811661d7cdf8e82186800715
Ivan Dwane <dwane.ivan@gmail.com>
2024-10-25 02:59:41 +0800

    components: temp add link helper

---
+++ src/components/helper/link.jsx
@@ -0,0 +1,18 @@
+export const Link = ({
+  href,
+  children,
+  isExternal = false,
+  isNewTab = false,
+  ...props
+}) => {
+  return (
+    <a
+      href={href}
+      rel={isExternal || !props.rel ? "noopener nofollow" : props.rel}
+      target={isNewTab || !props.target ? "_blank" : props.target}
+      {...props}
+    >
+      {children}
+    </a>
+  );
+};
