7f44587cfa3ed6cd98a2f598c14cfe38e5aa5b9e
Ivan Dwane <dwane.ivan@gmail.com>
2024-10-26 04:34:19 +0800

    feat: add dependencies for markdown rendering

---
+++ src/components/helper/markdown-renderer.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+
+import { marked } from "marked";
+import sanitizeHTML from "sanitize-html";
+
+import { cn } from "@/lib/utils";
+
+const MarkdownRenderer = ({
+  markdownRaw = "",
+  markedOptions = {},
+  sanitizeHTMLOptions = {},
+  className = "",
+}) => {
+  useEffect(() => {
+    marked.use(markedOptions);
+  }, [markedOptions]);
+
+  const getMarkdownText = () => {
+    const rawMarkdown = marked.parse(markdownRaw);
+    const sanitizedMarkdown = sanitizeHTML(rawMarkdown, sanitizeHTMLOptions);
+
+    return { __html: sanitizedMarkdown };
+  };
+
+  return (
+    <div
+      className={cn("prose", className)}
+      dangerouslySetInnerHTML={getMarkdownText()}
+    />
+  );
+};
+
+export default MarkdownRenderer;
