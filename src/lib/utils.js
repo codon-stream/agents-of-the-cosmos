4a16861ae2bdb8f1a750475378a85934476c67ee
Ivan Dwane <dwane.ivan@gmail.com>
2024-10-30 16:48:45 +0800

    feat: add utility function to generate URL slugs

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge";
 export function cn(...classValues) {
   return twMerge(clsx(classValues));
 }
+
+export const toURLSlug = (str) => {
+  return str
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^a-z0-9-]/g, "")
+    .replace(/^-+|-+$/g, "");
+};
