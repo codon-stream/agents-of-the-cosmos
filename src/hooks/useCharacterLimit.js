8efdf8c83a9799b1b5101f88184c5f857537446f
Ivan Dwane <dwane.ivan@gmail.com>
2024-11-01 01:25:49 +0800

    chore: export useCharacterLimit as a default function

--- src/hooks/useCharacterLimit.js
+++ src/hooks/useCharacterLimit.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
-const useCharacterLimit = ({ maxLength, initialValue = "" }) => {
+export default function useCharacterLimit({ maxLength, initialValue = "" }) {
   const [value, setValue] = useState(initialValue);
   const [characterCount, setCharacterCount] = useState(initialValue.length);
 
   const handleChange = (event) => {
     const newValue = event.target.value;
-    
+
     if (newValue.length <= maxLength) {
       setValue(newValue);
       setCharacterCount(newValue.length);
@@ -19,6 +19,4 @@ const useCharacterLimit = ({ maxLength, initialValue = "" }) => {
     handleChange,
     maxLength,
   };
-};
-
-export default useCharacterLimit;
+}
